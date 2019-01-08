816b9e4ac771f036f51ee758517e6bf7db9d8707
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-08 10:16:31 +0300

    Removed paginator from page

--- src/pages/issues-page.js
+++ src/pages/issues-page.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Issues from '../components/issues/issues-list'
 import { connect } from 'react-redux'
-import Paginator from '../components/issues/issues-paginator'
 import SearchField from '../components/search-field'
 import styles from './styles/issues-page.module.css'
 
 function IssuesPage({ match, locationKey }) {
   const { owner, repo, perpage, page } = match.params
+
   return (
     <div className={styles['container']}>
       <SearchField query={{ owner, repo }} key={locationKey} />
@@ -19,12 +19,6 @@ function IssuesPage({ match, locationKey }) {
           page={page}
         />
       </div>
-      <Paginator
-        owner={owner}
-        repo={repo}
-        currentPage={page}
-        perPage={perpage}
-      />
     </div>
   )
 }
