bb8d106960e5cc609b36df3948716bc4bcc96fed
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-08 10:17:04 +0300

    Added paginator

--- src/components/issues/issues-list.js
+++ src/components/issues/issues-list.js
@@ -4,11 +4,18 @@ import {
   fetchIssues,
   loadingSelector,
   issuesSelector,
+  lastPageSelector,
+  currentBasuUrlSelector,
+  goToPage,
 } from '../../ducks/issues'
 import IssuesListItem from './issues-list-item'
 import { redirectToIssue } from '../../ducks/single-issue'
 import styles from './styles/issues-list.module.css'
 import Loader from '../loader'
+import Paginator from '../paginator/paginator'
+
+//Tmp
+import { params1 } from '../paginator/paginator.stories'
 
 class Issues extends Component {
   constructor(props) {
@@ -24,7 +31,7 @@ class Issues extends Component {
     }
 
     return (
-      <>
+      <div className={styles['container']}>
         <ul className={styles['list']}>
           {issues.map(i => (
             <li
@@ -40,15 +47,45 @@ class Issues extends Component {
             </li>
           ))}
         </ul>
-      </>
+        <Paginator
+          params={{
+            ...params1,
+            baseUrl: this.props.baseUrl,
+            quantity: 6,
+            activePage: +this.props.page,
+            maxLimit: this.props.lastPage,
+          }}
+          onGoToPage={url => this.props.goToPage(url)}
+        />
+      </div>
     )
   }
 }
+
+/* 
+
+Paginator.propTypes = {
+  params: PropTypes.shape({
+    baseUrl: PropTypes.string.isRequired,
+    activePage: PropTypes.number.isRequired,
+    maxLimit: PropTypes.number.isRequired,
+    quantity: PropTypes.number.isRequired,
+    nextLinkText: PropTypes.string.isRequired,
+    prevLinkText: PropTypes.string.isRequired,
+    firstLinkText: PropTypes.string.isRequired,
+    lastLinkText: PropTypes.string.isRequired,
+  }),
+  onGoToPage: PropTypes.func.isRequired,
+}
+
+*/
 
 export default connect(
   state => ({
     loading: loadingSelector(state),
     issues: issuesSelector(state),
+    lastPage: lastPageSelector(state),
+    baseUrl: currentBasuUrlSelector(state),
   }),
-  { fetchIssues, redirectToIssue }
+  { fetchIssues, redirectToIssue, goToPage }
 )(Issues)
