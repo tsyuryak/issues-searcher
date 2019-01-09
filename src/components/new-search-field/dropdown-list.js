f120feff7c15b7406cbd24426f140ceab5f766b8
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-09 06:07:05 +0300

    added dropdown list style

--- src/components/new-search-field/dropdown-list.js
+++ src/components/new-search-field/dropdown-list.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styles from './dropdown-list.module.css'
 
 function DropdownList({ visible, repoes = [] }) {
   if (!visible) {
     return null
   }
   return (
-    <ul className="repo-list">
+    <ul className={styles['repo-list']}>
       {repoes.map(r => (
         <li key={r.id}>{r.name}</li>
       ))}
