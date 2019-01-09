f1d8b85a2c56e4e2310a794fa377c76faa6fa344
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-09 04:44:51 +0300

    added query item validator

--- src/components/new-search-field/utils.js
+++ src/components/new-search-field/utils.js
@@ -1,11 +1,17 @@
+export const queryItemIsValid = str => {
+  const validator = /^[A-Za-z]{1}\w+$/
+  return validator.test(str)
+}
+
 export const getOwnerFromQuery = query => {
   const lastSymb = query[query.length - 1]
-  const ownerRegExp = /^[A-Za-z]{1}\w+$/
   const separatorRegExp = /^(\s?|\/?)$/
   const owner =
-    ownerRegExp.test(query.slice(0, -1)) && separatorRegExp.test(lastSymb)
+    queryItemIsValid(query.slice(0, -1)) && separatorRegExp.test(lastSymb)
       ? query.slice(0, -1)
       : null
 
   return owner
 }
+
+export const getOwnerAndRepoByQuery = query => {}
