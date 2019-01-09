e9ff1235644ea5f183a8ba02b632b61a07083d54
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-09 05:31:22 +0300

    added proptypes

--- src/components/new-search-field/dropdown-list.js
+++ src/components/new-search-field/dropdown-list.js
@@ -1,8 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function DropdownList() {
+function DropdownList({ visible }) {
+  if (!visible) {
+    return null
+  }
   return <div>DDL</div>
+}
+
+DropdownList.propTypes = {
+  visible: PropTypes.bool.isRequired,
 }
 
 export default DropdownList
