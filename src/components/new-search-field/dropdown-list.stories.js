b02366f78c767616667de81b71495c82852cb7c3
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-09 06:49:59 +0300

    added params to visible droptown list in story

--- src/components/new-search-field/dropdown-list.stories.js
+++ src/components/new-search-field/dropdown-list.stories.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { createDropDownList } from './search-field.stories'
-
 import DropdownList from './dropdown-list'
 
 export const params1 = {
   visible: false,
   repoes: createDropDownList(10),
+  owner: '',
+}
+
+export const actions = {
+  onGoToRepo: action('onGoToRepo'),
 }
 
 storiesOf('Search Field > dropdown', module)
-  .add('default - invisible', () => <DropdownList {...params1} />)
+  .add('default - invisible', () => <DropdownList {...params1} {...actions} />)
   .add('repo list - visible', () => (
-    <DropdownList {...params1} visible={true} />
+    <DropdownList {...params1} visible={true} {...actions} owner="owner" />
   ))
