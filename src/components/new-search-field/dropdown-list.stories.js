73f7d53a604f7274a7af0dfe0c292cfc9530d412
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-09 05:41:51 +0300

    dropdown visible story

--- src/components/new-search-field/dropdown-list.stories.js
+++ src/components/new-search-field/dropdown-list.stories.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { createDropDownList } from './search-field.stories'
 
 import DropdownList from './dropdown-list'
 
 export const params1 = {
   visible: false,
+  repoes: createDropDownList(10),
 }
 
-storiesOf('Search Field > dropdown', module).add('default', () => (
-  <DropdownList {...params1} />
-))
+storiesOf('Search Field > dropdown', module)
+  .add('default - invisible', () => <DropdownList {...params1} />)
+  .add('repo list - visible', () => (
+    <DropdownList {...params1} visible={true} />
+  ))
