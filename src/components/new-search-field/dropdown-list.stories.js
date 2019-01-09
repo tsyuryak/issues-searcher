98a49c3b5035556b91edcb8d11a5d7af53f2b0e8
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-09 12:42:37 +0300

    added stories where items is active

--- src/components/new-search-field/dropdown-list.stories.js
+++ src/components/new-search-field/dropdown-list.stories.js
@@ -8,6 +8,7 @@ export const params1 = {
   visible: false,
   repoes: createDropDownList(10),
   owner: '',
+  activeItem: -1,
 }
 
 export const actions = {
@@ -17,5 +18,26 @@ export const actions = {
 storiesOf('Search Field > dropdown', module)
   .add('default - invisible', () => <DropdownList {...params1} {...actions} />)
   .add('repo list - visible', () => (
+    <DropdownList {...params1} visible={true} {...actions} owner="owner" />
+  ))
+  .add('repo list - active item #2', () => (
+    <DropdownList
+      {...params1}
+      visible={true}
+      activeItem={1}
+      {...actions}
+      owner="owner"
+    />
+  ))
+  .add('repo list - active item #5', () => (
+    <DropdownList
+      {...params1}
+      activeItem={4}
+      visible={true}
+      {...actions}
+      owner="owner"
+    />
+  ))
+  .add('repo list - unactive', () => (
     <DropdownList {...params1} visible={true} {...actions} owner="owner" />
   ))
