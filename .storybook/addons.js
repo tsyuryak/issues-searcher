86ac5da72649ed6350a7596d787ce8d2e5302e47
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-10 03:56:42 +0300

    added addons

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,2 +1,5 @@
+import addons from '@storybook/addons'
 import '@storybook/addon-actions/register'
 import '@storybook/addon-links/register'
+import registerRedux from 'addon-redux/register'
+registerRedux(addons)
