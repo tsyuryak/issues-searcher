0a875286a2e0e124fbb284b719bab3fa45ebcddd
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-11 01:54:34 +0300

    removed loading state

--- src/components/new-search-field/dropdown-list.stories.js
+++ src/components/new-search-field/dropdown-list.stories.js
@@ -13,7 +13,6 @@ export const actions = {
 }
 export const initialState = {
   loading: false,
-  loaded: false,
   visible: false,
   owner: '',
   repoes: [],
--- src/ducks/search-field.js
+++ src/ducks/search-field.js
@@ -5,15 +5,14 @@ import { createSelector } from 'reselect'
 export const moduleName = 'search-field'
 const prefix = `${appName}/${moduleName}`
 
+export const SET_TEST_VALUES = `${prefix}/SET_TEST_VALUES`
 export const SEARCH_ISSUES = `${prefix}/SEARCH_ISSUES`
 export const SEARCH_REPOES = `${prefix}/SEARCH_REPOES`
 export const GO_TO_REPO = `${prefix}/GO_TO_REPO`
-export const RESET_ACTIVE_ITEM = `${prefix}/RESET_ACTIVE_ITEM`
 export const SET_ACTIVE_ITEM = `${prefix}/SET_ACTIVE_ITEM`
 
 export const ReducerRecord = Record({
   loading: false,
-  loaded: false,
   visible: false,
   owner: '',
   repoes: [],
@@ -22,7 +21,19 @@ export const ReducerRecord = Record({
 })
 
 export default function reducer(state = ReducerRecord(), action) {
-  return state
+  switch (action.type) {
+    case SET_TEST_VALUES:
+      return state
+        .set('visible', action.values.visible)
+        .set('owner', action.values.owner)
+        .set('repoes', action.values.repoes)
+        .set('activeItem', action.values.activeItem)
+        .set('typedValue', action.values.typedValue)
+    case SET_ACTIVE_ITEM:
+      return state.set('activeItem', action.item)
+    default:
+      return state
+  }
 }
 
 //Selectors
@@ -31,11 +42,6 @@ export const stateSelector = state => state[moduleName]
 export const loadingSelector = createSelector(
   stateSelector,
   state => state.loading
-)
-
-export const loadedSelector = createSelector(
-  stateSelector,
-  state => state.loaded
 )
 
 export const visibleSelector = createSelector(
@@ -53,6 +59,11 @@ export const repoesSelector = createSelector(
   state => state.repoes
 )
 
+export const loadedSelector = createSelector(
+  repoesSelector,
+  repoes => repoes.length > 0
+)
+
 export const typedValueSelector = createSelector(
   stateSelector,
   state => state.typedValue
@@ -63,6 +74,24 @@ export const activeItemSelector = createSelector(
   state => state.activeItem
 )
 
+export const filteredRepoSelector = createSelector(
+  repoesSelector,
+  typedValueSelector,
+  (repoes, typedValue) => repoes.filter(r => r.name.includes(typedValue))
+)
+
+//AC
+
+//Only for STORYBOOK_MODE = true in .env
+export const setTestValues = values => ({
+  type: SET_TEST_VALUES,
+  values,
+})
+
+export const setActiveItem = item => ({
+  type: SET_ACTIVE_ITEM,
+  item,
+})
 /* 
   onSearchIssues: PropTypes.func.isRequired,
   onSearchRepoes: PropTypes.func.isRequired,
