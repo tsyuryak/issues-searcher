4c6f3965f2e7a7261086e56ce8071f24205635c6
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-10 06:11:46 +0300

    added redux

--- src/components/new-search-field/dropdown-list.stories.js
+++ src/components/new-search-field/dropdown-list.stories.js
@@ -2,14 +2,8 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
 import { createDropDownList } from './search-field.stories'
+import { withRedux } from '../../../.storybook/decorators'
 import DropdownList from './dropdown-list'
-
-export const params1 = {
-  visible: false,
-  repoes: createDropDownList(10),
-  owner: '',
-  activeItem: -1,
-}
 
 export const actions = {
   onGoToRepo: action('onGoToRepo'),
@@ -18,29 +12,62 @@ export const actions = {
   setListLength: action('setListLength'),
 }
 
-storiesOf('Search Field > dropdown', module)
-  .add('default - invisible', () => <DropdownList {...params1} {...actions} />)
-  .add('repo list - visible', () => (
-    <DropdownList {...params1} visible={true} {...actions} owner="owner" />
-  ))
-  .add('repo list - active item #2', () => (
-    <DropdownList
-      {...params1}
-      visible={true}
-      activeItem={1}
-      {...actions}
-      owner="owner"
-    />
-  ))
-  .add('repo list - active item #5', () => (
-    <DropdownList
-      {...params1}
-      activeItem={4}
-      visible={true}
-      {...actions}
-      owner="owner"
-    />
-  ))
-  .add('repo list - unactive', () => (
-    <DropdownList {...params1} visible={true} {...actions} owner="owner" />
-  ))
+const initialState = {
+  loading: false,
+  loaded: false,
+  visible: false,
+  owner: '',
+  repoes: [],
+  typedValue: '',
+  activeItem: -1,
+}
+
+const cannedActions = [{ name: 'Demo Action', action: { type: 'test' } }]
+
+storiesOf('Search Field/Dropdown', module)
+  .addDecorator(withRedux({}, []))
+  .add('invisible', () => <DropdownList />)
+
+storiesOf('Search Field/Dropdown', module)
+  .addDecorator(
+    withRedux(
+      {
+        ...initialState,
+        repoes: createDropDownList(10),
+        owner: 'owner',
+        visible: true,
+      },
+      []
+    )
+  )
+  .add('repo list - visible', () => <DropdownList />)
+
+storiesOf('Search Field/Dropdown', module)
+  .addDecorator(
+    withRedux(
+      {
+        ...initialState,
+        repoes: createDropDownList(10),
+        owner: 'owner',
+        activeItem: 1,
+        visible: true,
+      },
+      []
+    )
+  )
+  .add('repo list - active item #2', () => <DropdownList />)
+
+storiesOf('Search Field/Dropdown', module)
+  .addDecorator(
+    withRedux(
+      {
+        ...initialState,
+        repoes: createDropDownList(10),
+        owner: 'owner',
+        activeItem: 4,
+        visible: true,
+      },
+      []
+    )
+  )
+  .add('repo list - active item #5', () => <DropdownList />)
