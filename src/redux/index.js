fc8e2afae097d882ea7d345080da4de956a4eb11
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-10 04:12:46 +0300

    added redux enhancer

--- src/redux/index.js
+++ src/redux/index.js
@@ -2,6 +2,7 @@ import { createStore, applyMiddleware } from 'redux'
 import { routerMiddleware } from 'connected-react-router'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import createSagaMiddleware from 'redux-saga'
+import withReduxEnhancer from 'addon-redux/enhancer'
 import reducer from './reducer'
 import history from '../history'
 import rootSaga from './saga'
@@ -11,7 +12,11 @@ const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
   reducer(history),
   composeWithDevTools(
-    applyMiddleware(routerMiddleware(history), sagaMiddleware)
+    applyMiddleware(
+      routerMiddleware(history),
+      sagaMiddleware,
+      withReduxEnhancer
+    )
   )
 )
 
