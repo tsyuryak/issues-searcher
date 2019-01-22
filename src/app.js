fe86e492aad73af7ca75157ca4790984e8623941
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-22 13:19:28 +0300

    added pages

--- src/app.js
+++ src/app.js
@@ -1,34 +1,38 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-//import IssuesPage from './pages/issues-page'
-//import SingleIssuePage from './pages/single-issue-page'
-//import Status404 from './pages/status404-page'
+import SearchField from './components/search-field/search-field'
+import IssuesPage from './pages/issues-page'
+import SingleIssuePage from './pages/single-issue-page'
+import Status404 from './pages/status404-page'
 import HomePage from './pages/home-page'
 import styles from './styles/app.module.css'
 
 function App() {
   return (
     <div className={styles['app']}>
-      <Route path="/" component={HomePage} />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route
+          render={() => (
+            <>
+              <SearchField />
+              <Switch>
+                <Route
+                  path="/issues/:owner/:repo/:perpage(\d+)/:page(\d+)"
+                  component={IssuesPage}
+                />
+                <Route
+                  path="/repos/:owner/:repo/issues/:id(\d+)"
+                  component={SingleIssuePage}
+                />
+                <Route path="/404" component={Status404} />
+              </Switch>
+            </>
+          )}
+        />
+      </Switch>
     </div>
   )
 }
 
 export default App
-/* 
-
-<Switch>
-        <Route
-          path="/repos/:owner/:repo/issues/:id"
-          component={SingleIssuePage}
-        />
-        <Route
-          path="/issues/:owner/:repo/:perpage/:page"
-          component={IssuesPage}
-        />
-        <Route path="/issues/:owner/:repo" component={IssuesPage} />
-        <Route path="/404" component={Status404} />
-        <Route path="/" component={HomePage} />
-      </Switch>
-
-*/
