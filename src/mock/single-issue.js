7d15287151ca4176c47a707260e150ffb3785745
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-22 15:07:43 +0300

    added an issue mock

---
+++ src/mock/single-issue.js
@@ -0,0 +1,57 @@
+export const issue = {
+  url: 'https://api.github.com/repos/axios/axios/issues/1958',
+  repository_url: 'https://api.github.com/repos/axios/axios',
+  labels_url:
+    'https://api.github.com/repos/axios/axios/issues/1958/labels{/name}',
+  comments_url: 'https://api.github.com/repos/axios/axios/issues/1958/comments',
+  events_url: 'https://api.github.com/repos/axios/axios/issues/1958/events',
+  html_url: 'https://github.com/axios/axios/pull/1958',
+  id: 398805261,
+  node_id: 'MDExOlB1bGxSZXF1ZXN0MjQ0Mzc5MTk5',
+  number: 1958,
+  title: 'Allow opening examples in Gitpod',
+  user: {
+    login: 'svenefftinge',
+    id: 372735,
+    node_id: 'MDQ6VXNlcjM3MjczNQ==',
+    avatar_url: 'https://avatars0.githubusercontent.com/u/372735?v=4',
+    gravatar_id: '',
+    url: 'https://api.github.com/users/svenefftinge',
+    html_url: 'https://github.com/svenefftinge',
+    followers_url: 'https://api.github.com/users/svenefftinge/followers',
+    following_url:
+      'https://api.github.com/users/svenefftinge/following{/other_user}',
+    gists_url: 'https://api.github.com/users/svenefftinge/gists{/gist_id}',
+    starred_url:
+      'https://api.github.com/users/svenefftinge/starred{/owner}{/repo}',
+    subscriptions_url:
+      'https://api.github.com/users/svenefftinge/subscriptions',
+    organizations_url: 'https://api.github.com/users/svenefftinge/orgs',
+    repos_url: 'https://api.github.com/users/svenefftinge/repos',
+    events_url: 'https://api.github.com/users/svenefftinge/events{/privacy}',
+    received_events_url:
+      'https://api.github.com/users/svenefftinge/received_events',
+    type: 'User',
+    site_admin: false,
+  },
+  labels: [],
+  state: 'open',
+  locked: false,
+  assignee: null,
+  assignees: [],
+  milestone: null,
+  comments: 1,
+  created_at: '2019-01-14T09:26:51Z',
+  updated_at: '2019-01-21T10:01:14Z',
+  closed_at: null,
+  author_association: 'NONE',
+  pull_request: {
+    url: 'https://api.github.com/repos/axios/axios/pulls/1958',
+    html_url: 'https://github.com/axios/axios/pull/1958',
+    diff_url: 'https://github.com/axios/axios/pull/1958.diff',
+    patch_url: 'https://github.com/axios/axios/pull/1958.patch',
+  },
+  body:
+    'Added a button that allows opening the example project in Gitpod, a free online dev environment for GitHub. On start the dev environment will automatically run:\r\n\r\n1. `npm install`\r\n2. `grunt build`\r\n3. `npm run examples`\r\n\r\nA notification will pop up and ask you to open the examples website either in the preview or a new browser tab. \r\n\r\nYou can find the configuration here: \r\nhttps://github.com/gitpod-io/definitely-gp/blob/master/axios/.gitpod.yml\r\n\r\n<img width="1532" alt="screenshot 2019-01-14 at 10 26 11" src="https://user-images.githubusercontent.com/372735/51104583-d8479e80-17e6-11e9-8869-4a95e78e2592.png">\r\n',
+  closed_by: null,
+}
