839bb3aa06f2a65e89eb520c6e172f31006143de
Nikolai Tsiuriak <tsknik@gmail.com>
2019-01-31 02:29:51 +0300

    deleted test

--- src/pages/home-page.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { create } from 'react-test-renderer'
-import HomePage from './home-page'
-
-test('snapshot', () => {
-  const c = create(<HomePage />)
-  expect(c.toJSON()).toMatchSnapshot()
-})
