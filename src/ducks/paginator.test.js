c637525bd8447f065adc56a80205bdbb0aad3974
Nikolai Tsiuriak <tsknik@gmail.com>
2019-02-01 03:01:52 +0300

    added tests

--- src/ducks/paginator.test.js
+++ src/ducks/paginator.test.js
@@ -8,7 +8,18 @@ import {
   toFirstPage,
   baseUrlSelector,
   perPagePageSelector,
-  toFirstPageSaga,
+  maxLimitSelector,
+  activePageSelector,
+  toURLSaga,
+  TO_LAST_PAGE,
+  TO_PAGE_BY_NUM,
+  TO_PREV_PAGE,
+  TO_NEXT_PAGE,
+  TO_URL,
+  toLastPage,
+  toPageByNum,
+  toNextPage,
+  toPrevPage,
 } from './paginator'
 
 test('should create init action', () => {
@@ -32,16 +43,94 @@ test('should create toFirstPage action', () => {
 test('2', () => {
   const saga = testSaga(goToPageSaga)
     .next()
+    .take([
+      TO_FIRST_PAGE,
+      TO_LAST_PAGE,
+      TO_PAGE_BY_NUM,
+      TO_PREV_PAGE,
+      TO_NEXT_PAGE,
+      TO_URL,
+    ])
+
+  saga
+    .next(toFirstPage())
     .all({
       baseUrl: select(baseUrlSelector),
       perPage: select(perPagePageSelector),
+      maxLimit: select(maxLimitSelector),
+      activePage: select(activePageSelector),
     })
-    .next({ baseUrl: 'issues/owner/repo', perPage: 30 })
+    .next({
+      baseUrl: 'issues/owner/repo',
+      perPage: 30,
+    })
+    .fork(toURLSaga, 'issues/owner/repo/30/1')
+    .next()
 
   saga
-    .take(TO_FIRST_PAGE)
+    .next(toLastPage())
+    .all({
+      baseUrl: select(baseUrlSelector),
+      perPage: select(perPagePageSelector),
+      maxLimit: select(maxLimitSelector),
+      activePage: select(activePageSelector),
+    })
+    .next({
+      baseUrl: 'issues/owner/repo',
+      perPage: 30,
+      maxLimit: 20,
+    })
+    .fork(toURLSaga, 'issues/owner/repo/30/20')
     .next()
-    .fork(toFirstPageSaga, { baseUrl: 'issues/owner/repo', perPage: 30 })
+
+  saga
+    .next(toPageByNum(10))
+    .all({
+      baseUrl: select(baseUrlSelector),
+      perPage: select(perPagePageSelector),
+      maxLimit: select(maxLimitSelector),
+      activePage: select(activePageSelector),
+    })
+    .next({
+      baseUrl: 'issues/owner/repo',
+      perPage: 30,
+      activePage: 10,
+      maxLimit: 30,
+    })
+    .fork(toURLSaga, 'issues/owner/repo/30/10')
     .next()
-    .isDone()
+
+  saga
+    .next(toNextPage())
+    .all({
+      baseUrl: select(baseUrlSelector),
+      perPage: select(perPagePageSelector),
+      maxLimit: select(maxLimitSelector),
+      activePage: select(activePageSelector),
+    })
+    .next({
+      baseUrl: 'issues/owner/repo',
+      perPage: 30,
+      activePage: 2,
+      maxLimit: 30,
+    })
+    .fork(toURLSaga, 'issues/owner/repo/30/3')
+    .next()
+
+  saga
+    .next(toPrevPage())
+    .all({
+      baseUrl: select(baseUrlSelector),
+      perPage: select(perPagePageSelector),
+      maxLimit: select(maxLimitSelector),
+      activePage: select(activePageSelector),
+    })
+    .next({
+      baseUrl: 'issues/owner/repo',
+      perPage: 30,
+      activePage: 2,
+      maxLimit: 30,
+    })
+    .fork(toURLSaga, 'issues/owner/repo/30/1')
+    .next()
 })
